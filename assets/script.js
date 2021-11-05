8a9127c69c79d5574ac810677267e35a99f1ae13
Celeste Moore <celestealexmoore@gmail.com>
2021-11-05 01:22:36 -0500

    computer is able to choose, player is able to choose

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,20 @@
+// these objects create a boolean-like value for the onclick functionality
+let rock = {
+    "value" : false,
+    "numAssign" : 0
+};
+let paper = {
+    "value" : false,
+    "numAssign" : 1
+};
+let scissors = {
+    "value" : false,
+    "numAssign" : 2
+};
+let replay = {
+    "value" : false
+};
+
 // This controls the body's content
 let bodyEl = document.getElementById('bodyEl');
 
@@ -14,41 +31,169 @@ bannerEl.setAttribute(
     './assets/photos/banner.png'
 );
 
-// This controls the game element's float behavior
+// This controls the game elements' float behavior
 let gameEls = document.createElement('div');
 gameEls.classList.add('gameEls');
 
-// These are the individual game elements
+// Individual game elements
 let rockEl = document.createElement('img');
 rockEl.classList.add('images');
 rockEl.setAttribute(
     'src',
     './assets/photos/rock.png'
 );
-rockEl.onclick = function changeContent() {
-    rockEl.classList.add('hide');
-    return;
-}
-
 let paperEl = document.createElement('img');
 paperEl.classList.add('images');
 paperEl.setAttribute(
     'src',
     './assets/photos/paper.png'
 );
-paperEl.onclick = function changeContent() {
-    paperEl.classList.add('hide');
-    return;
-}
-
 let scissorsEl = document.createElement('img');
 scissorsEl.classList.add('images');
 scissorsEl.setAttribute(
     'src',
     './assets/photos/scissors.png'
 );
-scissorsEl.onclick = function changeContent() {
+
+
+// computer chooses a number between 0-1-2.
+let computerChoice = Math.floor(Math.random() * 3); //used the number 3, because math.random documentation says it will use 0-2. (which is 3 integers)
+console.log(computerChoice);
+
+
+// rock element's functionality
+rockEl.onclick = function playRock() {
+    // create a new div to replace rock when clicked
+    let resultDiv = document.createElement('div');
+    resultDiv.classList.add('gameEls');
+    let result = document.createElement('img');
+    let resultText = document.createElement('h1');
+
+    // switch the divs
+    rockEl.classList.add('hide');
+    result.classList.add('images');
+    resultText.classList.add('h1')
+
+    // creates a boolean-like value from object-variable, 'rock'.
+    // when rock is clicked, hide paper and scissors & show results.
+    rock["value"] = true;
+    if (rock["value"] = true) {
+        paperEl.classList.add('hide');
+        scissorsEl.classList.add('hide');
+    }
+
+    if (computerChoice === paper["numAssign"] && rock["value"] == true) {
+        console.log("Sorry! I chose paper.");
+        result.setAttribute(
+            'src',
+            './assets/photos/lost.png'
+        );
+    }else if (computerChoice === scissors["numAssign"] && rock["value"] == true) {
+        console.log("I chose scissors. You win!");
+        result.setAttribute(
+            'src',
+            './assets/photos/won.png'
+        );
+    }else {
+        console.log("It's a tie!")
+        result.setAttribute(
+            'src',
+            './assets/photos/tie.png'
+        );
+    };
+
+    bodyEl.appendChild(resultDiv);
+    resultDiv.appendChild(result);
+    // resultDiv.appendChild(resultText);
+    return;
+}
+
+// paper element's functionality
+paperEl.onclick = function playPaper() {
+    let resultDiv = document.createElement('div');
+    resultDiv.classList.add('gameEls');
+    let result = document.createElement('img')
+    result.setAttribute(
+        'src',
+        './assets/photos/chosePaper.png'
+    );
+    paperEl.classList.add('hide');
+    result.classList.add('images');
+
+    paper["value"] = true;
+    if (paper["value"] = true) {
+        rockEl.classList.add('hide');
+        scissorsEl.classList.add('hide');
+    }
+
+    if (computerChoice === scissors["numAssign"] && paper["value"] == true) {
+        console.log("Sorry! I chose scissors.");
+        result.setAttribute(
+            'src',
+            './assets/photos/lost.png'
+        );
+    }else if (computerChoice === rock["numAssign"] && paper["value"] == true) {
+        console.log("I chose rock. You win!");
+        result.setAttribute(
+            'src',
+            './assets/photos/won.png'
+        );
+    }else {
+        console.log("It's a tie!")
+        result.setAttribute(
+            'src',
+            './assets/photos/tie.png'
+        );
+    };
+    
+    bodyEl.appendChild(resultDiv);
+    resultDiv.appendChild(result);
+    // resultDiv.appendChild(resultText);
+    return;
+}
+
+// scissor element's functionality
+scissorsEl.onclick = function playScissors() {
+    let resultDiv = document.createElement('div');
+    resultDiv.classList.add('gameEls');
+    let result = document.createElement('img')
+    result.setAttribute(
+        'src',
+        './assets/photos/choseScissors.png'
+    );
+
     scissorsEl.classList.add('hide');
+    result.classList.add('images');
+
+    scissors["value"] = true;
+    if (scissors["value"] = true) {
+        rockEl.classList.add('hide');
+        paperEl.classList.add('hide');
+    }
+
+    if (computerChoice === rock["numAssign"] && scissors["value"] == true) {
+        console.log("Sorry! I chose rock.");
+        result.setAttribute(
+            'src',
+            './assets/photos/lost.png'
+        );
+    }else if (computerChoice === paper["numAssign"] && scissors["value"] == true) {
+        console.log("I chose paper. You win!");
+        result.setAttribute(
+            'src',
+            './assets/photos/won.png'
+        );
+    }else {
+        console.log("It's a tie!")
+        result.setAttribute(
+            'src',
+            './assets/photos/tie.png'
+        );
+    };
+
+    bodyEl.appendChild(resultDiv);
+    resultDiv.appendChild(result);
+    // resultDiv.appendChild(resultText);
     return;
 }
 
@@ -61,69 +206,11 @@ gameEls.appendChild(paperEl);
 gameEls.appendChild(scissorsEl);
 
 
-// functionality of the game.
-
-// let rock = 0;
-// let paper = 1;
-// let scissors = 2;
-
-// const consent = confirm("Do you wanna play?");
-// let letsPlay;
-
-// if (!consent) {
-//     console.log(consent);
-// } else {
-//     letsPlay = prompt(
-//         "Please choose Rock, Paper, or Scissors.",
-//         "insert answer here."
-//     ); //letsPlay == "rock"
-//   startGame(letsPlay.toLowerCase()); //letsPlay.toLowerCase() => "rock"
-//   //startGame("rock")
-// }
-
-// function verifyAnswer(input) {
-//     if (input != "rock" && input != "paper" && input != "scissors") {
-//     let newInput = prompt(
-//     "Please choose Rock, Paper, or Scissors.",
-//     "try again."
-//     );
-//     newInput = newInput.toLowerCase();
-//     startGame(newInput);
-//     } else {
-//     return;
-//     }
-// };
-
-//     function startGame(input) {
-//       //startGame("rock") => input == "rock"
-//     verifyAnswer(input);
-
-//     let computerChoice = Math.floor(Math.random() * 3); //used the number 4, because math.random documentation says it will use 0-3.
-//     console.log(computerChoice);
-
-//     if (computerChoice == rock && input == "paper") {
-//     alert("I chose rock. You win!");
-//     }else if (computerChoice == paper && input == "scissors") {
-//     alert("I chose paper. You win!");
-//     }else if (computerChoice == scissors && input == "rock") {
-//     alert("I chose scissors. You win!");
-//     }else if (computerChoice == rock && input == "scissors") {
-//     alert("Sorry! I chose rock.");
-//     }else if (computerChoice == paper && input == "rock") {
-//     alert("Sorry! I chose paper.");
-//     }else if (computerChoice == scissors && input == "paper") {
-//     alert("Sorry! I chose scissors.");
-//     }else {
-//     alert("It's a tie!")
-//     };
-//     return;
-// };
-
 //end of game.
-  //game is giving me several rounds although I only wanted one.
 
 
-// 4. Write Algorithm for computer to choose between r, p, or s at random.
+
+
 // 5. Write if-statement: "if (computer feedback r && user feedback s)":
 // 6. else if: "if (computer feedback p && user feedback r)":
 // 7. else if: "if (computer feedback s && user feedback p)":
